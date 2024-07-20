b5547102b0215c2832a1c22f9bf6ec9f8c4f86c8
igarashi-naoki <naoki17igarashi@gmail.com>
2024-07-20 13:36:35 +0900

    ポイント計算ロジックのリファクタとセッターメソッド、呼び出し元の変更

--- chapter1/movie.rb
+++ chapter1/movie.rb
@@ -3,24 +3,12 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title, :price_code
+  attr_reader :title
+  attr_writer :price
 
-  def price_code=(value)
-    @price_code = value
-    @price =
-      case price_code
-      when REGULAR
-        RegularPrice.new
-      when NEW_RELEASE
-        NewReleasePrice.new
-      when CHILDRENS
-        ChildrensPrice.new
-      end
-  end
-
-  def initialize(title, the_price_code)
+  def initialize(title, price)
     @title = title
-    self.price_code = the_price_code
+    @price = price
   end
 
   def charge(days_rented)
@@ -28,11 +16,19 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-    price_code == Movie::NEW_RELEASE && days_rented > 1 ? 2 : 1
+    @price.frequent_renter_points(days_rented)
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(_days_rented)
+    1
   end
 end
 
 class RegularPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -41,12 +37,18 @@ class RegularPrice
 end
 
 class NewReleasePrice
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+
   def charge(days_rented)
     days_rented * 3
   end
 end
 
 class ChildrensPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -7,17 +7,17 @@ require_relative '../chapter1/rental'
 class CustomerTest < Minitest::Test
   def test_statement
     customer = Customer.new('John')
-    movie = Movie.new('Star Wars', Movie::REGULAR)
+    movie = Movie.new('Star Wars', RegularPrice.new)
     rental = Rental.new(movie, 3)
     customer.add_rental(rental)
     assert_equal "Rental Record for John\n\tStar Wars\t3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points", customer.statement
 
-    movie_avatar = Movie.new('Avatar', Movie::NEW_RELEASE)
+    movie_avatar = Movie.new('Avatar', NewReleasePrice.new)
     rental_avatar = Rental.new(movie_avatar, 5)
     customer.add_rental(rental_avatar)
     assert_equal "Rental Record for John\n\tStar Wars\t3.5\n\tAvatar\t15\nAmount owed is 18.5\nYou earned 3 frequent renter points", customer.statement
 
-    movie_toy_story = Movie.new('Toy Story', Movie::CHILDRENS)
+    movie_toy_story = Movie.new('Toy Story', ChildrensPrice.new)
     rental_toy_story = Rental.new(movie_toy_story, 2)
     customer.add_rental(rental_toy_story)
     assert_equal "Rental Record for John\n\tStar Wars\t3.5\n\tAvatar\t15\n\tToy Story\t1.5\nAmount owed is 20.0\nYou earned 4 frequent renter points", customer.statement
