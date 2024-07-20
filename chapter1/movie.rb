18dc690b8f620ad70669e222157e0b23783c3835
igarashi-naoki <naoki17igarashi@gmail.com>
2024-07-20 13:09:31 +0900

    料金、ポイント計算をmovieクラスに移動

--- chapter1/movie.rb
+++ chapter1/movie.rb
@@ -10,4 +10,23 @@ class Movie
     @title = title
     @price_code = price_code
   end
+
+  def charge(days_rented)
+    result = 0
+    case price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
+  def frequent_renter_points(days_rented)
+    price_code == Movie::NEW_RELEASE && days_rented > 1 ? 2 : 1
+  end
 end
