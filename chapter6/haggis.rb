9a386242ca621c30bdbdd52e4d89b90b2831ad76
igarashi-naoki <naoki17igarashi@gmail.com>
2024-07-21 02:28:17 +0900

    haggisクラスのリファクタリング

--- chapter6/haggis.rb
+++ chapter6/haggis.rb
@@ -4,15 +4,36 @@ class Haggis
   end
 
   def distance_traveled(time)
-    primary_acc = @primary_force / @mass
-    primary_time = [time, @delay].min
-    result = 0.5 * primary_acc * primary_time * primary_time
-    secondary_time = time - @delay
-    if secondary_time
-      primary_vel = primary_acc * @delay
-      secondary_acc = (@primary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
+    if secondary_time(time) > 0
+      total_distance = primary_distance(primary_time(time)) + primary_vel * secondary_time(time) + 5 *
+                       secondary_acc * secondary_time(time) * secondary_time(time)
     end
-    result
+    total_distance
+  end
+
+  private
+
+  def primary_acc
+    @primary_force / @mass
+  end
+
+  def primary_distance(primary_time)
+    0.5 * primary_acc * primary_time * primary_time
+  end
+
+  def primary_time(time)
+    [time, @delay].min
+  end
+
+  def secondary_time(time)
+    time - @delay
+  end
+
+  def secondary_acc
+    (@primary_force + @secondary_force) / @mass
+  end
+
+  def primary_vel
+    primary_acc * @delay
   end
 end
