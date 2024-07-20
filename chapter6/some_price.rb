bad8bd38317e815cd59f04722c0d091450239aa7
igarashi-naoki <naoki17igarashi@gmail.com>
2024-07-21 01:47:21 +0900

    一時変数への代入をメソッド化

--- chapter6/some_price.rb
+++ chapter6/some_price.rb
@@ -5,12 +5,18 @@ class SomePrice
   end
 
   def price
-    base_price = @quantity * @item_price
-    if base_price > 1000
+    a_base_price = base_price
+    if a_base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
     end
-    base_price * discount_factor
+    a_base_price * discount_factor
+  end
+
+  private
+
+  def base_price
+    @quantity * @item_price
   end
 end
