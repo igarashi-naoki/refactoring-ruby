042c5e499b01eb2a83fd5073362bf96dedc301b4
igarashi-naoki <naoki17igarashi@gmail.com>
2024-07-20 23:48:30 +0900

    テストケースの追加

--- chapter4/test/file_test.rb
+++ chapter4/test/file_test.rb
@@ -5,4 +5,27 @@ class FileReaderTest < Test::Unit::TestCase
     contents = File.read('chapter4/test/fixtures/sample.txt')
     assert_equal 'd', contents[3, 1]
   end
+
+  def test_read_with_a_length_specified
+    contents = File.read('chapter4/test/fixtures/sample.txt', 15)
+    assert_equal 'Bradman 99.94 5', contents
+  end
+
+  def test_read_empty_file
+    File.open('chapter4/test/fixtures/empty.txt', 'w') {}
+    contents = File.read('chapter4/test/fixtures/empty.txt')
+    assert_equal '', contents
+  end
+
+  def test_read_an_out_of_bounds_length_causes_no_error
+    File.open('chapter4/test/fixtures/simple.txt', 'w') { |file| file << 'simple file' }
+    contents = File.read('chapter4/test/fixtures/simple.txt', 100)
+    assert_equal 'simple file', contents
+  end
+
+  def test_read_raises_error_when_file_no_found
+    assert_raises Errno::ENOENT do
+      File.read('chapter4/test/fixtures/x.txt')
+    end
+  end
 end
